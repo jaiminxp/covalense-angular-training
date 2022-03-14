53d18344b43ebd8913942c1a4e42ebfb944aefe3
Jaimin <jaimins100@gmail.com>
2022-03-14 12:52:07 +0530

    form validations: template forms

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { TemplateFormsModule } from './app/concepts/template-forms/template-forms.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {

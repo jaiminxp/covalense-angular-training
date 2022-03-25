98c0160411da2e41e24af2cab34424379a8a03f9
Jaimin <jaimins100@gmail.com>
2022-03-25 12:22:15 +0530

    Binding to directive properties

--- src/app/concepts/custom-directives/better-highlight.directive.ts
+++ src/app/concepts/custom-directives/better-highlight.directive.ts
@@ -1,24 +1,41 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  HostBinding,
+  Input,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective {
+  @HostBinding('style.backgroundColor') backgroundColor = '';
+
+  @Input() highlightColor: string;
+
   constructor(private el: ElementRef, private renderer: Renderer2) {
     // el.nativeElement.style.backgroundColor = 'yellow';
-    this.renderer.setStyle(el.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(el.nativeElement, 'background-color', 'transparent');
+
+    this.backgroundColor = 'tranparent';
   }
 
   @HostListener('mouseenter') addHighlight() {
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'cyan');
+    // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'cyan');
+    this.backgroundColor = this.highlightColor;
+    console.log('🚀 ~ this.highlightColor', this.highlightColor);
   }
 
   @HostListener('mouseleave') removeHighlight() {
-    this.renderer.setStyle(
-      this.el.nativeElement,
-      'background-color',
-      'transparent'
-    );
+    // this.renderer.setStyle(
+    //   this.el.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    // );
+
+    this.backgroundColor = 'transparent';
   }
 
   @HostListener('click') clickHost() {
--- src/app/concepts/custom-directives/c1/c1.component.ts
+++ src/app/concepts/custom-directives/c1/c1.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-c1',
   templateUrl: './c1.component.html',
-  styleUrls: ['./c1.component.css']
+  styleUrls: ['./c1.component.css'],
 })
 export class C1Component implements OnInit {
+  color: string = 'lightpink';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

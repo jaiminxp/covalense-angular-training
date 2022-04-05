b35f3b08e6ffcb2e43ae08d41e530d05693727cc
Jaimin <jaimins100@gmail.com>
2022-04-05 12:35:09 +0530

    Form validation using observables

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,15 +15,81 @@ export class ContactComponent implements OnInit {
   @ViewChild('fform') feedbackFormDirective: any;
 
   constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  formErrors: any = {
+    firstname: '',
+    lastname: '',
+    telnum: '',
+    email: '',
+  };
+
+  validationMessages: any = {
+    firstname: {
+      required: 'First Name is required',
+      minlength: 'First Name must be atleast 2 characters long',
+      maxlength: 'First Name cannot be more than 25 characters long',
+    },
+    lastname: {
+      required: 'Last Name is required.',
+      minlength: 'Last Name must be at least 2 characters long.',
+      maxlength: 'Last Name cannot be more than 25 characters long.',
+    },
+    telnum: {
+      required: 'Tel. number is required.',
+      pattern: 'Tel. number must contain only numbers.',
+    },
+    email: {
+      required: 'Email is required.',
+      email: 'Email not in valid format.',
+    },
+  };
+
+  createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(25),
+        ],
+      ],
+      lastname: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(25),
+        ],
+      ],
+      telnum: ['', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: '',
     });
+
+    this.feedbackForm.valueChanges.subscribe((data) => this.onValueChanged());
+  }
+
+  onValueChanged() {
+    if (!this.feedbackForm) {
+      return;
+    }
+
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const error in control.errors) {
+          this.formErrors[field] = messages[error] + '';
+        }
+      }
+    }
   }
 
   ngOnInit(): void {}

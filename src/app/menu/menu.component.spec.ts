e3d8dfbcec2caf8589a8fe11ff803a2a8e34bd98
Jaimin <jaimins100@gmail.com>
2022-04-08 14:50:56 +0530

    testing menu component

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,54 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MenuComponent } from './menu.component';
+import { DishService } from '../services/dish.service';
+import { Observable, of } from 'rxjs';
+import { Dish } from '../shared/Dish';
+import { DISHES } from '../shared/dishes';
+import { baseurl } from '../shared/baseurl';
+import { DebugElement } from '@angular/core';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
+  const dishServiceStub = {
+    getDishes: (): Observable<Dish[]> => {
+      console.log('🚀 ~ DISHES', DISHES);
+
+      return of(DISHES);
+    },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
-    })
-    .compileComponents();
+      declarations: [MenuComponent],
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        MatProgressSpinnerModule,
+        FlexLayoutModule,
+        MatGridListModule,
+        RouterTestingModule.withRoutes([
+          {
+            path: 'menu',
+            component: MenuComponent,
+          },
+        ]),
+      ],
+      providers: [
+        { provide: DishService, useValue: dishServiceStub },
+        { provide: 'BaseURL', useValue: baseurl },
+      ],
+    }).compileComponents();
+
+    const dishservice = TestBed.inject(DishService);
   });
 
   beforeEach(() => {
@@ -21,5 +59,22 @@ describe('MenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('dishes should be 4', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[0].name).toBe('Uthappizza');
+  });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
   });
 });
--- src/test.ts
+++ src/test.ts
@@ -25,6 +25,6 @@ getTestBed().initTestEnvironment(
 );
 
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('./', true, /menu\.component\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
